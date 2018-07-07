9be10f71b937513958639460babb0560610dc691
nmzz <071bct541@pcampus.edu.np>
2018-07-07 23:36:18 +0545

    before async

--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 var express = require('express'),
     config = require('./server/configure'),
     app = express();
+    mongoose = require ('mongoose');
 // set default 
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
+
+mongoose.connect ('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open',function(){
+    console.log('Mongoose connected.');
+});
 // Create a HTTP server using app object to listen for connections
 // app.get('/', function (req, res) {
 //     res.send('Hello World');
@@ -13,3 +19,4 @@ app = config(app);
 var server= app.listen(app.get('port'), function () {
     console.log('Server up : http://localhost:' + app.get('port'));
 });
+
